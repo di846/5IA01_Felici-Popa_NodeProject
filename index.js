ff0b73bf6fe81453760e42aae2ec8794865bd365
Ubuntu <ubuntu@vps-6eb7800f.vps.ovh.net>
2025-02-21 09:47:00 +0000

    Primo script js + .gitignore

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Ciao, il server Express sta funzionando!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server in ascolto sulla porta: {port}`);
+});
