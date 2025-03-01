d23463310d22d9d47e8cfdba5e6c786b62ea842c
di846 <163345336+di846@users.noreply.github.com>
2025-03-01 16:55:56 +0100

    Ennesimo test workflow

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Ciao, il server Express sta funzionando!')
+  res.send('Il server Express sta funzionando!')
 })
 
 
